67feba17570dba3b83fa2a639aa7e618d8f1fb6a
Alec Johnson <alecjohnson55@gmail.com>
2017-03-26 19:35:04 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './lib/mandala_maker.jsx',
+  output: {
+    filename: './lib/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
