6a672464c94866c469d5f58f78169a9f663f8f38
Alec Johnson <alecjohnson55@gmail.com>
2017-03-28 17:17:08 -0700

    remove react from presets for babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015']
         }
       }
     ]
